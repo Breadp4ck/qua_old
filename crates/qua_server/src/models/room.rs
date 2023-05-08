e6415efbb1b360688ee1792ba0d40ba6567b8b0f
Breadp4ck <iii103@mail.ru>
2023-05-08 22:46:48 +0700

    server room logic update

--- crates/qua_server/src/models/room.rs
+++ crates/qua_server/src/models/room.rs
@@ -1,6 +1,6 @@
 use axum::extract::ws::{Message, WebSocket};
 use futures::{sink::SinkExt, stream::StreamExt};
-use qua_game::{game::Game, package::prelude::Package, person::Person};
+use qua_game::{game::{Game, GameEvent}, package::prelude::Package, person::Person};
 
 pub struct Room {
     game: Game,
@@ -26,7 +26,7 @@ impl Room {
                 if let Ok(message) = message {
                     match message {
                         Message::Text(text) => {
-                            let event = serde_json::from_str::<Event>(&text).unwrap();
+                            let event = serde_json::from_str::<GameEvent>(&text).unwrap();
                         }
                         Message::Binary(_) => {}
                         Message::Ping(_) => (),
