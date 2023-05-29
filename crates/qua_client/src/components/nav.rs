fbcd8568229577ef50781214cca6de0e52208473
Breadp4ck <iii103@mail.ru>
2023-05-29 23:24:50 +0700

    copy room code to clipboard by clicking it

--- crates/qua_client/src/components/nav.rs
+++ crates/qua_client/src/components/nav.rs
@@ -1,17 +1,26 @@
 use dioxus::prelude::*;
 use dioxus_router::*;
 use fermi::use_read;
+use web_sys::window;
 
 use crate::ROOM_CODE;
 
 pub fn nav(cx: Scope) -> Element {
     let room_code = use_read(cx, ROOM_CODE);
 
-    let room_code = if let Some(room_code) = room_code {
-        Some(room_code.to_string())
-    } else {
-        None
+    let copy_code = move |_| {
+        to_owned!(room_code);
+
+        cx.spawn({
+            async move {
+                if let Some(room_code) = room_code {
+                    let clipboard = window().unwrap().navigator().clipboard().unwrap();
+                    clipboard.write_text(&room_code.to_string());
+                }
+            }
+        });
     };
+
 
     cx.render(rsx! {
         nav {
@@ -20,12 +29,13 @@ pub fn nav(cx: Scope) -> Element {
             Link { to: "/menu", class: "to-left", img { class: "nav-btn-icon", src: "assets/icons/home-outline.svg", alt: "P" } }
             // Right side of the navbar
             Link { to: "/notifications", class: "to-right", img { class: "nav-btn-icon", src: "assets/icons/info-outline.svg", alt: "P" } }
-            if room_code.is_some() {
+
+            if let Some(room_code) = room_code {
                 rsx!(
-                    Link {
-                        to: "/game-info",
+                    div {
+                        onclick: copy_code,
                         class: "to-right impressive",
-                        "{room_code.unwrap().to_string()}"
+                        "{room_code.to_string()}"
                     },
                     Link {
                         to: "/room",
